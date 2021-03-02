9990fb0587f3e3dca8f4d2006d2b1b7f1ad481b2
oranmoshe <oran.moshe@correnet.com>
2021-03-02 14:09:59 +0200

    update

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,13 +7,7 @@
 
 let changeColor = document.getElementById('changeColor');
 
-function modifyDOM() {
-	var h1 = document.getElementsByTagName('h1')[0];
-	var aa = document.getElementsByClassName('css-eaycls');        
-  return aa[aa.length-1].innerText +"/"+ h1.innerText.replace(/\W/g, '-').replace(/ /g, "-").
-  replace(/---/g, "-").
-  replace(/--/g, "-").replace(/"/g, "").replace(/'/g, "").toLowerCase();
-}
+
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
@@ -21,14 +16,12 @@ chrome.storage.sync.get('color', function(data) {
 
 changeColor.onclick = function(element) {
   let color = element.target.value;
-      //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
-    chrome.tabs.executeScript({
-        code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
-    }, (results) => {
-        //Here we have just the innerHTML and not DOM structure
-        console.log('Popup script:')
-        prompt("Branch name", results[0]);
-    });
-
-
+  
+	chrome.windows.getCurrent(function (currentWindow) {
+	    chrome.tabs.query({ active: true, windowId: currentWindow.id }, function (activeTabs) {
+	        activeTabs.map(function (tab) {
+	            chrome.tabs.executeScript(tab.id, { file: 'content-script.js', allFrames: false });
+	        });
+	    });
+	});
 };
